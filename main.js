af7ae6d5689befa516603f65042c818c984dac19
mattnorthcutt <matt_northcutt@icloud.com>
2025-01-23 19:58:14 -0600

    scope

--- main.js
+++ main.js
@@ -41,13 +41,7 @@ console.log(HTMLRepresentation)
 const locations = [[1,1], [1,2], [1,3], [2,1], [2,2], [2,3]]
 
 for (const location of locations) {
-    let invalidLocation = false;
-
-    if (location[0] > 2) {
-        invalidLocation = true
-    }
-
-    if (invalidLocation = true) {
-        console.log("This location is invalid")
-    }
+   if (location[0] > 2) {
+    console.log(`This location is invalid: ${location}`)
+   }
 }
